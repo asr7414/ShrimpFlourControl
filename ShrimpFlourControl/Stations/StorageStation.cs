f289d045052aa4b9e7cb6b253a528dca1d38b740
DESKTOP-6JOBUT7\tony9 <tony940001@gmail.com>
2022-10-16 00:56:14 +0800

    Rack Storage WIP Load Unload

--- ShrimpFlourControl/Stations/StorageStation.cs
+++ ShrimpFlourControl/Stations/StorageStation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ShrimpFlourControl.Maps;
 
@@ -12,6 +13,18 @@ namespace ShrimpFlourControl.Stations
         public StorageStation(int id, Node refererNode, int offsetX, int offsetY, char remark) : base(id, refererNode, offsetX, offsetY, StationType.StorageStation, remark)
         {
 
+        }
+        public void LoadWorkPiece()
+        {
+            this.Status = StationStatus.Loading;
+            Thread.Sleep(1000);
+            this.Status = StationStatus.Idle;
+        }
+        public void UnloadWorkPiece()
+        {
+            this.Status = StationStatus.Unloading;
+            System.Threading.Thread.Sleep(1000);
+            this.Status = StationStatus.Idle;
         }
     }
 }
